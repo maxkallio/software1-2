c3c4a99cb5c83e6c08d8c56dcc04bbe6d5cb0612
maxkallio <maxkallio7@gmail.com>
2023-02-03 02:29:20 +0200

    8

--- 8.2.py
+++ 8.2.py
@@ -1,11 +1,12 @@
 import mysql.connector
-from geopy.distance import geodesic as GD
 
 def request(a):
     kursori = server.cursor()
     kursori.execute(a)
     tulos = kursori.fetchall()
+    server.close()
     return tulos
+
 
 server = mysql.connector.connect(
          host='127.0.0.1',
@@ -16,9 +17,22 @@ server = mysql.connector.connect(
          autocommit=True
          )
 
-ident = [input("input aiport code"), input("input airport code")]
-result = list()
-for item in ident:
-       result.append(request("SELECT latitude_deg, longitude_deg from airports where ident like '"+str(item)+"';"))
-print(f"{GD(result[0][0],result[1][0]).km} km")
-server.close()
+country = input("type country code ")
+q = "SELECT type from airports where iso_country like '"+str(country)+"';"
+result = request(q)
+balloonport_count = 0
+closed_count = 0
+heliport_count = 0
+small_airport_count = 0
+seaplane_base_count = 0
+medium_airport_count = 0
+large_airport_count = 0
+airports = ['balloonport', 'closed', 'heliport', 'small_airport', 'seaplane_base', 'medium_airport', 'medium_airport']
+airports_vars = [balloonport_count, closed_count, heliport_count, small_airport_count,  seaplane_base_count, medium_airport_count, large_airport_count]
+for item in result:
+    item = list(item)
+    for i,name in enumerate(airports):
+        if(item[0] == name):
+            airports_vars[i] += 1
+for i,name in enumerate(airports):
+    print(f"{name}: {airports_vars[i]}")
